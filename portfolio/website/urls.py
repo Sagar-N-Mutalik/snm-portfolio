714a95bb315f58cc142905fddf0cc31ecd118dd6
sagar n mutalik <sagarnmutalik6@gmailcom>
2026-03-12 12:15:55 +0530

    updates

--- portfolio/website/urls.py
+++ portfolio/website/urls.py
@@ -1,12 +1,9 @@
 from django_distill import distill_path
 from .views import HomeView, ProjectsView, LeadershipView
 
-def get_none():
-    """Returns None as there are no dynamic URL parameters for these pages."""
-    return None
-
 urlpatterns = [
-    distill_path('', HomeView.as_view(), name='home', distill_func=get_none),
-    distill_path('projects/', ProjectsView.as_view(), name='projects', distill_func=get_none),
-    distill_path('leadership/', LeadershipView.as_view(), name='leadership', distill_func=get_none),
+    # Removed distill_func completely for static pages with no dynamic parameters
+    distill_path('', HomeView.as_view(), name='home'),
+    distill_path('projects/', ProjectsView.as_view(), name='projects'),
+    distill_path('leadership/', LeadershipView.as_view(), name='leadership'),
 ]
